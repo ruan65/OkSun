6e30d615bff545524f5c98e4aa25fb28cc697b5b
ruan65 <ruan65@gmail.com>
2015-06-18 02:27:07 +0300

    detailed fragment implemented

---
+++ mobile/src/main/java/idea/ruan/oksun/DetailFragment.java
@@ -0,0 +1,198 @@
+package idea.ruan.oksun;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import idea.ruan.oksun.data.WeatherContract;
+
+import static idea.ruan.oksun.data.WeatherContract.WeatherEntry;
+
+public class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String FORECAST_SHARE_HASHTAG = "#OkSunApp";
+
+    private static final int FORECAST_LOADER_ID = 0xEba;
+
+    private ShareActionProvider mShareActionProvider;
+
+    private String mForecast;
+
+    DetailActivity ctx;
+
+    private static final String[] DETAIL_COLUMNS = {
+
+            WeatherEntry.TABLE_NAME + "." + WeatherEntry._ID,
+            WeatherEntry.COLUMN_DATE,
+            WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherEntry.COLUMN_HUMIDITY,
+            WeatherEntry.COLUMN_PRESSURE,
+            WeatherEntry.COLUMN_WIND_SPEED,
+            WeatherEntry.COLUMN_DEGREES,
+            WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING
+    };
+
+    private static final int COL_WEATHER_ID = 0;
+    private static final int COL_WEATHER_DATE = 1;
+    private static final int COL_WEATHER_DESC = 2;
+    private static final int COL_WEATHER_MAX_TEMP = 3;
+    private static final int COL_WEATHER_MIN_TEMP = 4;
+    private static final int COL_WEATHER_HUMIDITY = 5;
+    private static final int COL_WEATHER_PRESSURE = 6;
+    private static final int COL_WEATHER_WIND_SPEED = 7;
+    private static final int COL_WEATHER_DEGREES = 8;
+    public static final int COL_WEATHER_CONDITION_ID = 9;
+
+    private ImageView mIconView;
+    private TextView mFriendlyDateView;
+    private TextView mDateView;
+    private TextView mDescriptionView;
+    private TextView mHighTempView;
+    private TextView mLowTempView;
+    private TextView mHumidityView;
+    private TextView mWindView;
+    private TextView mPressureView;
+
+
+    public DetailFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.f_detail, container, false);
+        mIconView = (ImageView) rootView.findViewById(R.id.detail_icon);
+        mDateView = (TextView) rootView.findViewById(R.id.detail_date_textview);
+        mFriendlyDateView = (TextView) rootView.findViewById(R.id.detail_day_textview);
+        mDescriptionView = (TextView) rootView.findViewById(R.id.detail_forecast_textview);
+        mHighTempView = (TextView) rootView.findViewById(R.id.detail_high_textview);
+        mLowTempView = (TextView) rootView.findViewById(R.id.detail_low_textview);
+        mHumidityView = (TextView) rootView.findViewById(R.id.detail_humidity_textview);
+        mWindView = (TextView) rootView.findViewById(R.id.detail_wind_textview);
+        mPressureView = (TextView) rootView.findViewById(R.id.detail_pressure_textview);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ctx = (DetailActivity) activity;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(FORECAST_LOADER_ID, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.m_f_detail, menu);
+
+        MenuItem item = menu.findItem(R.id.action_share);
+
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        if (mForecast != null) {
+
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
+    }
+
+    private Intent createShareForecastIntent() {
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, FORECAST_SHARE_HASHTAG);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecast);
+
+        return shareIntent;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        Intent intent = getActivity().getIntent();
+
+        if (intent == null) return null;
+
+        return new CursorLoader(ctx, intent.getData(), DETAIL_COLUMNS, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+
+        if (c == null || !c.moveToFirst()) return;
+
+        int weatherId = c.getInt(COL_WEATHER_DATE);
+
+        mIconView.setImageResource(R.mipmap.ic_launcher);
+
+        long date = c.getLong(COL_WEATHER_DATE);
+        String friendlyDateText = Utility.getDayName(ctx, date);
+        String dateText = Utility.getFormattedMonthDay(ctx, date);
+
+        mFriendlyDateView.setText(friendlyDateText);
+        mDateView.setText(dateText);
+
+        String description = c.getString(COL_WEATHER_DESC);
+        mDescriptionView.setText(description);
+
+        boolean isMetric = Utility.isMetric(getActivity());
+        double high = c.getDouble(COL_WEATHER_MAX_TEMP);
+        String highString = Utility.formatTemperature(getActivity(), high, isMetric);
+
+        mHighTempView.setText(highString);
+
+        double low = c.getDouble(COL_WEATHER_MIN_TEMP);
+        String lowString = Utility.formatTemperature(ctx, low, isMetric);
+
+        mLowTempView.setText(lowString);
+
+        float humidity = c.getFloat(COL_WEATHER_HUMIDITY);
+        mHumidityView.setText(ctx.getString(R.string.format_humidity, humidity));
+
+        float windSpeedStr = c.getFloat(COL_WEATHER_WIND_SPEED);
+        float windDirStr = c.getFloat(COL_WEATHER_DEGREES);
+
+        mWindView.setText(Utility.getFormattedWind(ctx, windSpeedStr, windDirStr));
+
+        float pressure = c.getFloat(COL_WEATHER_PRESSURE);
+        mPressureView.setText(ctx.getString(R.string.format_pressure, pressure));
+
+        mForecast = String.format("%s - %s - %s/%s", dateText, description, high, low);
+
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+}
--- mobile/src/main/java/idea/ruan/oksun/ForecastAdapter.java
+++ mobile/src/main/java/idea/ruan/oksun/ForecastAdapter.java
@@ -6,9 +6,13 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import static idea.ruan.oksun.ForecastFragment.*;
+import static idea.ruan.oksun.ForecastFragment.COL_WEATHER_DATE;
+import static idea.ruan.oksun.ForecastFragment.COL_WEATHER_DESC;
+import static idea.ruan.oksun.ForecastFragment.COL_WEATHER_MAX_TEMP;
+import static idea.ruan.oksun.ForecastFragment.COL_WEATHER_MIN_TEMP;
 
 /**
  * {@link ForecastAdapter} exposes a list of weather forecasts
@@ -16,62 +20,76 @@ import static idea.ruan.oksun.ForecastFragment.*;
  */
 public class ForecastAdapter extends CursorAdapter {
 
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
-    /**
-     * Prepare the weather high/lows for presentation.
-     */
-    private String formatHighLows(double high, double low) {
-        boolean isMetric = Utility.isMetric(mContext);
-        String highLowStr = Utility.formatTemperature(high, isMetric) + "/"
-                + Utility.formatTemperature(low, isMetric);
-        return highLowStr;
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
     }
 
-    /*
-        This is ported from FetchWeatherTask --- but now we go straight from the cursor to the
-        string.
-     */
-    private String convertCursorRowToUXFormat(Cursor cursor) {
-
-        String highAndLow = formatHighLows(
-                cursor.getDouble(COL_WEATHER_MAX_TEMP),
-                cursor.getDouble(COL_WEATHER_MIN_TEMP));
-
-        return Utility.formatDate(cursor.getLong(COL_WEATHER_DATE)) +
-                " - " + cursor.getString(COL_WEATHER_DESC) +
-                " - " + highAndLow;
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
     }
 
-    /*
-        Remember that these views are reused as needed.
-     */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
 
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        int itemLayoutId = getItemViewType(cursor.getPosition()) == VIEW_TYPE_TODAY ?
+                R.layout.list_item_forecast_today : R.layout.list_item_forecast;
+
+        View returnedView = LayoutInflater.from(context).inflate(itemLayoutId, parent, false);
+        returnedView.setTag(new ViewHolder(returnedView));
+
+        return returnedView;
     }
 
-    /*
-        This is where we fill-in the views with the contents of the cursor.
-     */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // our view is pretty simple here --- just a text view
-        // we'll keep the UI functional with a simple (and slow!) binding.
 
-        ((TextView)view.findViewById(R.id.list_item_forecast_textview))
-                .setText(cursor.getString(COL_WEATHER_DESC));
+        ViewHolder h = (ViewHolder) view.getTag();
 
-        ((TextView)view.findViewById(R.id.list_item_date_textview))
-                .setText(cursor.getString(COL_WEATHER_DATE));
+        int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
 
-        ((TextView)view.findViewById(R.id.list_item_high_textview))
-                .setText(cursor.getString(COL_WEATHER_MAX_TEMP));
+        h.iconView.setImageResource(R.mipmap.ic_launcher);
 
-        ((TextView)view.findViewById(R.id.list_item_low_textview))
-                .setText(cursor.getString(COL_WEATHER_MIN_TEMP));
+        h.descriptionView.setText(cursor.getString(COL_WEATHER_DESC));
+
+        long dateInMillis = Long.parseLong(cursor.getString(COL_WEATHER_DATE));
+
+        h.dateView.setText(Utility.getFriendlyDayString(context, dateInMillis));
+
+        boolean isMetric = Utility.isMetric(context);
+
+        h.high.setText(Utility.formatTemperature(
+                mContext, cursor.getDouble(COL_WEATHER_MAX_TEMP), isMetric));
+
+        h.low.setText(Utility.formatTemperature(
+                mContext, cursor.getDouble(COL_WEATHER_MIN_TEMP), isMetric));
+    }
+
+    /**
+     * Cache of the children views for a forecast list item.
+     */
+    public static class ViewHolder {
+        public final ImageView iconView;
+        public final TextView dateView;
+        public final TextView descriptionView;
+        public final TextView high;
+        public final TextView low;
+
+        public ViewHolder(View view) {
+            iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+            dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+            descriptionView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+            high = (TextView) view.findViewById(R.id.list_item_high_textview);
+            low = (TextView) view.findViewById(R.id.list_item_low_textview);
+        }
     }
 }
