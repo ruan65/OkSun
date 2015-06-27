0cfb3ed85d6ef30301a95f55bcb99a10955627d2
ruan65 <ruan65@gmail.com>
2015-06-27 04:32:15 +0300

    wanna see changes

--- mobile/src/main/java/idea/ruan/oksun/Main.java
+++ mobile/src/main/java/idea/ruan/oksun/Main.java
@@ -4,13 +4,10 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 
 public class Main extends AppCompatActivity implements ForecastFragment.Callback {
 
