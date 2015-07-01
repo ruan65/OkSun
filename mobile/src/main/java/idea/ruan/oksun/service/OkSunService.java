e71b4db0dcb5277a07ad1f4a79d29c19872cf5e3
ruan65 <ruan65@gmail.com>
2015-07-01 08:34:43 +0300

    adding service

---
+++ mobile/src/main/java/idea/ruan/oksun/service/OkSunService.java
@@ -0,0 +1,16 @@
+package idea.ruan.oksun.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class OkSunService extends IntentService {
+
+    public OkSunService() {
+        super("OkSunService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
